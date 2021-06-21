47e9365ee042f61c5b463367d817ef2dd0049ad8
Brad Guner <bradley.guner@priceline.com>
2021-06-21 15:19:29 -0400

    Add Countxir Server.

---
+++ countxir/lib/countxir/server.ex
@@ -0,0 +1,39 @@
+defmodule Countxir.Server do
+  use GenServer
+  alias Countxir.Core
+
+  #Client
+  def start_link(num) when is_binary(num) do
+    GenServer.start_link(__MODULE__, num)
+  end
+
+  def inc(counter) do
+    GenServer.cast(counter, :inc)
+  end
+
+  def dec(counter) do
+    GenServer.cast(counter, :dec)
+  end
+
+  def read(counter) do
+    GenServer.call(counter, :read)
+  end
+
+
+  #Server
+  def init(num) do
+    {:ok, Core.new(num)}
+  end
+
+  def handle_call(:read, _from, num) do
+    {:reply, Core.message(num), num}
+  end
+
+  def handle_cast(:inc, num) do
+    {:noreply, Core.add(num, 1)}
+  end
+
+  def handle_cast(:dec, num) do
+    {:noreply, Core.add(num, -1)}
+  end
+end
