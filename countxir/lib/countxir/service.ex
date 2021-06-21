f9901c516df233c8b7a69b908d40102736d197df
Marcos Ribeiro <marcos.ribeiro@priceline.com>
2021-06-21 13:54:31 -0400

    my commit

--- countxir/lib/countxir/service.ex
+++ countxir/lib/countxir/service.ex
@@ -4,15 +4,28 @@ defmodule Countxir.Service do
   def start(string) do
     spawn(fn -> string |> Core.new |> run end)
   end
+
+  def inc(counter) do
+    send(counter, :inc)
+  end
+
+  def read(counter) do
+    send(counter, {:read, self()})
+    receive do
+      string -> string
+    end
+  end
+
   def run(num) do
     num |> listen() |> run()
   end
+
   def listen(num) do
     receive do
       :inc ->
         Core.add(num, 1)
       {:read, from} ->
-        send(from, num)
+        send(from, Core.message(num))
         num
     end
   end
