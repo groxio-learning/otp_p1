37244e60c79c6b8b22cf3a2bc872e9278a52a223
Brad Guner <bradley.guner@priceline.com>
2021-06-22 11:52:30 -0400

    Adding Enigma Score class with a test case.

--- enigma/lib/enigma/score.ex
+++ enigma/lib/enigma/score.ex
@@ -1,3 +1,21 @@
 defmodule Enigma.Score do
 
+  defstruct [:red, :white]
+
+  def new(answer, move) do
+    %__MODULE__{red: reds(answer, move), white: whites(answer, reds(answer, move), misses(answer, move))}
+  end
+
+  defp reds(answer, move) do
+    Enum.count(Enum.zip(answer, move), fn {x,y} -> x == y end)
+  end
+
+  defp misses(answer, move) do
+    Enum.count(move -- answer)
+  end
+
+  defp whites(answer, red, miss) do
+    Enum.count(answer) - red - miss
+  end
+
 end
