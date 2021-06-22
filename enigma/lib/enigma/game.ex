d542b33f1072f9ac3c1a37149736382be6e043e7
Nariman Saftarli <nariman.saftarli@priceline.com>
2021-06-22 11:34:10 -0400

    Define game structure and reduce

--- enigma/lib/enigma/game.ex
+++ enigma/lib/enigma/game.ex
@@ -1,3 +1,11 @@
 defmodule Enigma.Game do
+  defstruct [:moves, :answer]
 
+  def move(game, guess) do
+    %__MODULE__{answer: game.answer, moves: [guess | game.moves]}
+  end
+
+  def new(answer) do
+    %__MODULE__{answer: answer, moves: []}
+  end
 end
