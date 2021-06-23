d49e7673d407ca5833028f15dc6297abded42e74
Nariman Saftarli <nariman.saftarli@priceline.com>
2021-06-23 14:13:17 -0400

    Add multiple child processes

--- enigma/lib/enigma.ex
+++ enigma/lib/enigma.ex
@@ -2,14 +2,18 @@ defmodule Enigma do
   use GenServer
   alias Enigma.Game
 
-  def start_link(starting_value) do
-    IO.puts("Make a guess (e.g. Enigma.guess([1,2,3,4])")
-    GenServer.start_link(__MODULE__, starting_value, name: __MODULE__)
+  def start_link({starting_value, name}) do
+    IO.puts("Make a guess, #{name} (e.g. Enigma.guess([1,2,3,4])")
+    GenServer.start_link(__MODULE__, starting_value, name: name || __MODULE__)
   end
 
   def guess(game \\ __MODULE__, guess) do
     GenServer.call(game, {:guess, guess})
     |> IO.puts
+  end
+
+  def child_spec({_starting_value, name}) do
+    %{id: name, start: {Enigma, :start_link, [{nil, name}]}}
   end
 
   def init(starting_value) do
@@ -20,5 +24,4 @@ defmodule Enigma do
     new_game = Game.move(game, guess)
     {:reply, Game.to_map(new_game), new_game}
   end
-
 end
--- enigma/lib/enigma/application.ex
+++ enigma/lib/enigma/application.ex
@@ -9,12 +9,16 @@ defmodule Enigma.Application do
   def start(_type, _args) do
     children = [
       # Starts a worker by calling: Enigma.Worker.start_link(arg)
-      {Enigma, nil}
+      {Enigma, {nil, :bruce}},
+      {Enigma, {nil, :veera}},
+      {Enigma, {nil, :brad}},
+      {Enigma, {nil, :marcos}},
+      {Enigma, {nil, :nariman}}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Enigma.Supervisor]
+    opts = [strategy: :rest_for_one, name: Enigma.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
